c0e03610224020633b323f860aea08a9d50fd4dc
none <none@none.none>
2020-01-22 19:03:55 +0100

    Basic storage and retrieval using REST calls

--- src/main/java/io/gottaofast/images/controller/ImageController.java
+++ src/main/java/io/gottaofast/images/controller/ImageController.java
@@ -1,5 +1,6 @@
 package io.gottaofast.images.controller;
 
+import io.gottaofast.images.ImageProcessor;
 import io.gottaofast.images.model.request.ProcessRequest;
 import io.gottaofast.images.model.response.IDResponse;
 import io.gottaofast.images.model.response.ProcessResponse;
@@ -9,7 +10,6 @@ import java.util.UUID;
 
 @RestController
 public class ImageController {
-    String base64Image = "";
     /**
      * The <code>@GetMapping</code> annotation ensures that HTTP GET requests to <code>/filters</code>
      * are mapped to the <code>filters()</code> method.
@@ -20,19 +20,16 @@ public class ImageController {
      */
     @PostMapping("/images/process")
     public ProcessResponse process(@RequestBody ProcessRequest payload) {
-        base64Image = payload.getImage();
-//        System.out.println(new String(Base64.getDecoder().decode(base64Image.getBytes())));
-//        byte[] imageBytes = javax.xml.bind.DatatypeConverter.parseBase64Binary(base64Image);
 
         // image UUID for retrieval later
         ProcessResponse response = new ProcessResponse(UUID.randomUUID());
-        // TODO: send UUID back, get filter configuration, filter image, store filtered image at said uuid
+        ImageProcessor.saveImageToFiles(payload.getImage(), response.getImgID().toString());
 
         return response;
     }
 
     @GetMapping(value = "images/id/{imageID}")
     public IDResponse retrieve(@PathVariable String imageID) {
-        return new IDResponse(base64Image);
+        return new IDResponse(ImageProcessor.loadBase64ImageFromFiles(imageID));
     }
 }
